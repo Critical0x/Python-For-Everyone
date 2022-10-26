7a19296062fa1bd6bf1b8933770fbeb8ef5c2b16
Critical0x <103599644+Critical0x@users.noreply.github.com>
2022-10-26 00:36:08 -0400

    Python for Everybody Course - Chapter 4

---
+++ ch4.py
@@ -0,0 +1,29 @@
+def nl():
+    return print('\n')
+
+
+# Parameters #
+
+#def greet(lang):
+    #if lang == 'es':
+        #print("Hola")
+    #elif lang == 'fr':
+        #print("Bonjour")
+    #else:
+        #print("Hello")
+
+#greet('es')
+#greet('fr')
+#greet('en')
+
+
+# Return Values # 
+
+#def greet():
+    #return 'Hello'
+
+#print(greet(), 'Archibald')
+#print(greet(), 'Marsha')
+#nl()
+#print(greet(), 'Harold')
+
